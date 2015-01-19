2bdfb56ff179bfa0f6113fa5622d0b0891d0da82
MICHAEL MACKINNON <mike@eth0.ca>
2015-01-19 12:12:32 -0700

    removed erroneous imports

--- apps/shifts/urls.py
+++ apps/shifts/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, RedirectView
 from django.http import HttpResponseRedirect
 
-from .views import ShiftListView, ShiftCreateView, ShiftUpdateView, ShiftListCalendarView, ShiftListCreateUpdateAPIView,\
-    ShiftDeleteAPIView, ShiftDeleteView
+from .views import ShiftListView, ShiftCreateView, ShiftUpdateView, ShiftListCalendarView, ShiftListCreateUpdateAPIView
 
 
 urlpatterns = \
@@ -12,7 +11,6 @@ urlpatterns = \
 
              url(r'^$', ShiftListView.as_view(), name='shift_list'),
              url(r'^shift/edit/(?P<pk>\d+)/$', ShiftUpdateView.as_view(), name='shift_update'),
-             url(r'^shift/delete/$', ShiftDeleteView.as_view(), name='shift_delete'),
              url(r'^calendar/delete/$', 'apps.shifts.views.delete_shift_from_calendar', name='shift_delete_calendar'),
              url(r'^calendar/$', ShiftListCalendarView.as_view(), name='shift_calendar'),
              url(r'^shift/', ShiftCreateView.as_view(), name='shift_create'),
