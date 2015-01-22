642d3fef0019d197d73d47197c85f16d430c282c
MICHAEL MACKINNON <mike@eth0.ca>
2015-01-22 11:30:38 -0700

    enable facebook

--- shiftgap/settings.py
+++ shiftgap/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'allauth.account',
     'allauth.socialaccount',
     # ... include the providers you want to enable:
-    # 'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
     'allauth.socialaccount.providers.twitter',
     # 'allauth.socialaccount.providers.github',
