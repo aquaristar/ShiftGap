0e799412d24a1d441fa4ef54215e60cead8df421
MICHAEL MACKINNON <mike@eth0.ca>
2015-01-27 20:22:26 -0700

    request.user is a simplelazyobject until used

--- apps/ui/context_processors.py
+++ apps/ui/context_processors.py
@@ -11,6 +11,6 @@ def process_ui_views(request):
             'time': arrow.utcnow().datetime,
             'naive': timezone.is_aware(arrow.utcnow().datetime),
             'yourtz': request.session.get('django_timezone', None),
-            'myshifts': Shift.objects.filter(user=request.user, published=True,
+            'myshifts': Shift.objects.filter(user__pk=request.user.pk, published=True,
                                              start_time__gte=arrow.utcnow().datetime).order_by('start_time')[0:10]
     }
