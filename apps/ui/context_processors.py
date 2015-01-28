2ec3aab78266084d08e69c9ce6d82943fc081241
MICHAEL MACKINNON <mike@eth0.ca>
2015-01-27 18:39:49 -0700

    show upcoming shifts on welcome screen

--- apps/ui/context_processors.py
+++ apps/ui/context_processors.py
@@ -3,10 +3,14 @@ import arrow
 
 from django.utils import timezone
 
+from apps.shifts.models import Shift
+
 
 def process_ui_views(request):
     return {'build': environ.get('BUILD_HASH', None),
             'time': arrow.utcnow().datetime,
             'naive': timezone.is_aware(arrow.utcnow().datetime),
             'yourtz': request.session.get('django_timezone', None),
-            }
+            'myshifts': Shift.objects.filter(user=request.user, published=True,
+                                             start_time__gte=arrow.utcnow().datetime).order_by('start_time')[0:10]
+    }
