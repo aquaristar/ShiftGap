4de375d5b22ede6703f40e09dcc4128b582e4d4f
MICHAEL MACKINNON <mike@eth0.ca>
2015-02-12 10:10:52 -0700

    added french PO file for upcoming meeting with translator

--- apps/shifts/templatetags/running_total.py
+++ apps/shifts/templatetags/running_total.py
@@ -8,7 +8,7 @@ register = template.Library()
 def running_total(objects_by_user):
     """
     Get the running total of the hours (duration of each shift) by user grouping
-    :param objects_by_day:
+    :param objects_by_user:
     :return:
     """
     return sum(d.duration for d in objects_by_user)
